53111475175b9a22bfb43509a5f598c526d8b91b
MATE <MATE@MATE-PC>
2016-02-18 09:32:20 +0100

    INICIO

---
+++ src/myapp/Libreta.java
@@ -0,0 +1,28 @@
+package myapp;
+
+import javax.swing.JOptionPane;
+
+public class Libreta {
+    
+    /**
+     * 
+     */
+    private String nombre;
+    private String DNI;
+    private String Apellido;
+
+    public void añadir() {
+        JOptionPane.showMessageDialog(null, "Pedir nombre y apellido");
+        JOptionPane.showConfirmDialog(null, "Pedir DNI");
+        JOptionPane.showConfirmDialog(null, "Pedir");
+        
+    }
+
+    @Override
+    public String toString() {
+        return "Libreta{" + "nombre=" + nombre + ", DNI=" + DNI + ", Apellido=" + Apellido + '}';
+        
+        
+    }
+
+}
